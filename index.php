45a86b106395aa4330a5093ba752f1c99c0c6364
Aidan Smith <originalaidan@gmail.com>
2025-01-20 15:06:28 -0700

    added modern styling - still needs work - fully functional

--- index.php
+++ index.php
@@ -7,18 +7,20 @@
     <link rel="stylesheet" href="calc.css">
 </head>
 <body>
-    <div class="page-header">
-        <h1>Tipping Calculator</h1>
-        <h2>Please enter your bill details</h2>
+    <div class="content-wrapper">
+        <div class="page-header">
+            <h1>Tipping Calculator</h1>
+            <h2>Please enter your bill details</h2>
+        </div>
+        <form method="POST" action="results.php">
+            <label for="billAmount">Bill Total</label>
+            <input type="number" name="billAmount" placeholder="Enter bill amount">
+            <label for="guests">Number of Guests</label>
+            <input type="number" name="guests" placeholder="Enter total guests">
+            <label for="tipPercentage">Tip Percentage</label>
+            <input type="number" name="tipPercentage" placeholder="Enter tip percentage" min="15" value="15">
+            <input class="submit-btn" type="submit" value="Split the Bill!">
+        </form>
     </div>
-    <form method="POST" action="results.php">
-        <label for="billAmount">Bill Total</label>
-        <input type="number" name="billAmount" placeholder="Enter bill amount">
-        <label for="guests">Number of Guests</label>
-        <input type="number" name="guests" placeholder="Enter total guests">
-        <label for="tipPercentage">Tip Percentage</label>
-        <input type="number" name="tipPercentage" placeholder="Enter tip percentage" min="15" value="15">
-        <input type="submit" value="Split the Bill!">
-    </form>
 </body>
 </html>
--- results.php
+++ results.php
@@ -4,19 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tipping Calculator | Results</title>
+    <link rel="stylesheet" href="calc.css">
 </head>
-<body>
-    <h1>Tipping Calculator</h1>
-    <h2>Your current bill summary</h2>
-    <?php 
-        $billAmount = ($_POST['billAmount']);
-        $guestAmount = ($_POST['guests']);
-        $tipPercentage = ($_POST['tipPercentage']);
-        $splitTotal = 0;
+<body class="results">
+    <div class="content-wrapper">
+        <div class="page-header">
+            <h1>Tipping Calculator</h1>
+            <h2>Your current bill summary</h2>
+        </div>
+        <?php 
+            $billAmount = ($_POST['billAmount']);
+            $guestAmount = ($_POST['guests']);
+            $tipPercentage = ($_POST['tipPercentage']);
+            $splitTotal = 0;
 
-        $totalTip = $billAmount * ($tipPercentage / 100);
-        $splitTotal = ($totalTip + $billAmount) / $guestAmount;
-    ?>
-    <p>The total each person owes is $<?php echo number_format($splitTotal, 2)?>.</p>
+            $totalTip = $billAmount * ($tipPercentage / 100);
+            $splitTotal = ($totalTip + $billAmount) / $guestAmount;
+        ?>
+        <p>The total each person owes is $<?php echo number_format($splitTotal, 2)?>.</p>
+    </div>
 </body>
 </html>
